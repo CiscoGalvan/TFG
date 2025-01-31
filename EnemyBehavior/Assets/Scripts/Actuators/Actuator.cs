19367192049a4b416e01e6185a8b26dc4dbc223c
CiscoGalvan <fragalva@ucm.es>
2025-01-31 13:27:31 +0100

    Removiendo archivos ignorados

--- EnemyBehavior/Assets/Scripts/Actuators/Actuator.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-
-//clase base de la que heredan todos los actuadores
-public class Actuator : MonoBehaviour
-{
-}
